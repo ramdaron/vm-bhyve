e457767109bc2914ad24d56989552bc95b52a644
Matt Churchyard <churchers@gmail.com>
2015-11-18 15:30:56 +0000

    Try alpine with new linux method

---
+++ sample-templates/grub/alpine-boot.cmd
@@ -0,0 +1,4 @@
+linux /boot/vmlinuz-grsec root=/dev/vda3 modules=ext4
+initrd /boot/initramfs-grsec
+boot
+
---
+++ sample-templates/grub/alpine-iso.cmd
@@ -0,0 +1,4 @@
+linux /boot/grsec initrd=/boot/initramfs-grsec alpine_dev=cdrom:iso9660 modules=loop,squashfs,sd-mod,usb-storage,sr-mod
+initrd /boot/initramfs-grsec
+boot
+
